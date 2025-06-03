76eab3cff9f79d7aeaad0469b72f51ba97565683
rkvst <vimalkannan8138@gmail.com>
2025-06-03 11:13:32 +0530

    Update Viewstory.jsx

--- src/Viewstory.jsx
+++ src/Viewstory.jsx
@@ -1,36 +1,45 @@
-import React, { useEffect, useState } from 'react'
-import { useParams,Link, useNavigate } from 'react-router-dom'
-function Viewstory() {
-    const { id,tot} = useParams();
+import React, { useEffect, useState } from 'react';
+import { useParams, Link, useNavigate } from 'react-router-dom';
 
-    const [Story, setStory] = useState(null);
-    const navigate=useNavigate();
-    useEffect(() => {
-        fetch(`(https://my-json-server.typicode.com/rkvst/instagram-clone/story/${id}`)
-            .then(data => data.json())
-            .then((data) => {
-        console.log("Fetched story:", data); // ✅ Debug
+function Viewstory() {
+  const { id, tot } = useParams();
+  const [story, setStory] = useState(null);
+  const navigate = useNavigate();
+
+  const baseUrl ='https://my-json-server.typicode.com/rkvst/instagram-clone';
+    
+
+  useEffect(() => {
+    fetch(`${baseUrl}/story/${id}`)
+      .then((data) => data.json())
+      .then((data) => {
+        console.log("Fetched story:", data);
         setStory(data);
       })
-            .catch(err => console.log(err))
-    }, [id])
+      .catch((err) => console.log(err));
+  }, [id]);
 
-    if(id>tot || id<=0){
-       navigate('/');
-    }
-    return (
-        <div>
-            {Story ? (
-                <div className='h-screen flex justify-center items-center'>
-                    <Link to={`http://localhost:5173/story/${Number(id)-1}/${tot}`}><i className='bx bx-arrow-back'></i></Link>
-                 <img className='max-w-full h-auto' src={Story.image} />
-                      <Link to={`http://localhost:5173/story/${Number(id)+1}/${tot}`}><i class='bx bx-arrow-back bx-rotate-180'></i></Link>
-                </div>
-            ) : (
-                <p>Loading</p>
-            )}
+  if (id > tot || id <= 0) {
+    navigate('/');
+  }
+
+  return (
+    <div>
+      {story ? (
+        <div className="h-screen flex justify-center items-center gap-5">
+          <Link to={`/story/${Number(id) - 1}/${tot}`}>
+            <i className="bx bx-arrow-back text-3xl"></i>
+          </Link>
+          <img className="max-w-full h-auto rounded-lg" src={story.image} alt="story" />
+          <Link to={`/story/${Number(id) + 1}/${tot}`}>
+            <i className="bx bx-arrow-back bx-rotate-180 text-3xl"></i>
+          </Link>
         </div>
-    )
+      ) : (
+        <p className="text-center mt-10">Loading...</p>
+      )}
+    </div>
+  );
 }
 
-export default Viewstory
+export default Viewstory;
